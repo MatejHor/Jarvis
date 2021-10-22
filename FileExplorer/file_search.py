b93c5403a7f018a3ddf0fc98c031491f4d325398
MatejHor <mat.horniak@gmail.com>
2021-10-22 16:52:59 +0200

    Update file_search.py

--- FileExplorer/file_search.py
+++ FileExplorer/file_search.py
@@ -40,29 +40,52 @@ def command_run(command, stdout=True):
     return result_.stdout
 
 
-def search(file):
+def search(file, path):
     if platform.system() == "Windows":
-        [windows_search(file=file, path=drive + "\\\\") for drive in get_drives()]
+        # print(get_drives())
+        if path:
+            windows_search(file=file, path=path + "\\\\")
+        else:
+            [windows_search(file=file, path=drive + "\\\\") for drive in get_drives()]
     else:
-        linux_search(file=file)
+        linux_search(file=file, path=path)
+
+def findall(directory):
+    files=os.listdir(directory)
+    for fl in files:
+        path=os.path.join(directory,fl)
+        if os.path.isdir(path):
+            findall(path)
+        else:
+            # TODO dosomethingwithfile(path)
+            pass
+    return
 
 
 parser = argparse.ArgumentParser(description='Python file searching')
 parser.add_argument('file', metavar='file', nargs=1, type=str,
                     help='searching file, word')
-parser.add_argument('-er', '--end-regex', action='store_true',
-                    help='searching for file which start with define word')
-parser.add_argument('-sr', '--start-regex', action='store_true',
-                    help='searching for file which end with define work')
+parser.add_argument('-er', '--end-regex', action='store_true', dest='end_regex',
+                    help='searching for file and add wild card (after word could be any letter) on end')
+parser.add_argument('-sr', '--start-regex', action='store_true', dest='start_regex',
+                    help='searching for file and add wild card (before word could be any letter) on start')
+parser.add_argument('-r', '--full-regex', action='store_true', dest='full_regex',
+                    help='searching for file and add wild card arround the letters')
+parser.add_argument('-p', '--path', dest='path',
+                    help='Path')
+    
 
 args = parser.parse_args()
 file_ = args.file[0]
+path_ = args.path
 try:
     if args.end_regex:
         file_ = file_ + "*"
     if args.start_regex:
         file_ = "*" + file_
-    print('Searching ->', file_)
-    search(file_)
+    if args.full_regex:
+        file_ = "*" + file_ + "*"    
+    print('[+] Searching ->', file_)
+    search(file_, path=path_)
 except Exception as e:
-    print('file_search: ' + str(e))
+    print('[-] file_search: ' + str(e))
